5d530373fc999053f1e7e9383d2adc76f920f635
sfincks <sfincks666@gmail.com>
2023-02-05 16:36:20 +0500

    zadacha_01

--- Zadacha_01/Program.cs
+++ Zadacha_01/Program.cs
@@ -6,4 +6,43 @@
 // 23432 -> да
 // 12821 -> да
 
+int InputNumber(string num)
+{
+    System.Console.WriteLine(num);
+    return Convert.ToInt32(Console.ReadLine());
+}
+start:
+int number = InputNumber("Введите пятизначное число");
 
+if (number > 0 && (number < 10000 || number > 99999))
+{
+    System.Console.WriteLine("Вы ввели не пятизначное число, попробуйте ещё раз");
+    goto start;
+}
+else if (number > 9999 && number < 100000)
+{
+    int[] array = new int[5];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        int result = number;
+        while (result >= (Math.Pow(10, i + 1)))
+        {
+            result = result / 10;
+        }
+        array[i] = result % 10;
+        System.Console.Write(array[i] + " ");
+    }
+    if ((array[0] == array[4]) && (array[1] == array[3]))
+    {
+        System.Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        System.Console.WriteLine("Число не является палиндромом");
+    }
+}
+else if (number == 00000)
+{
+    System.Console.WriteLine("Число является палиндромом");
+}
